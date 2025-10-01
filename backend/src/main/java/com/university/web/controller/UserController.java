063027d48fb061adf6bda79ff9f0ceb8afe00e1b
PatriickRM <patriickrs201210@gmail.com>
2025-09-30 23:03:57 -0500

    add usercontroller

---
+++ backend/src/main/java/com/university/web/controller/UserController.java
@@ -0,0 +1,94 @@
+package com.university.web.controller;
+
+import com.university.application.service.UserService;
+import com.university.domain.model.enums.UserStatus;
+import com.university.web.dto.user.UserCreateRequestDTO;
+import com.university.web.dto.user.UserResponseDTO;
+import com.university.web.dto.user.UserUpdateRequestDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    //Crear usuario
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserCreateRequestDTO dto) {
+        UserResponseDTO response = userService.createUser(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    //Obtener usuario por id
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @userSecurityService.isOwner(#id)")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        UserResponseDTO response = userService.getUserById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    //Obtener todos los usuarios
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> response = userService.getAllUsers();
+        return ResponseEntity.ok(response);
+    }
+
+    // Obtener usuarios paginados
+    @GetMapping("/paginated")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<UserResponseDTO>> getAllUsersPageable(
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<UserResponseDTO> response = userService.getAllUsersPageable(pageable);
+        return ResponseEntity.ok(response);
+    }
+
+    //Buscar usuarios por status
+    @GetMapping("/status/{status}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserResponseDTO>> getUsersByStatus(@PathVariable UserStatus status) {
+        List<UserResponseDTO> response = userService.getUsersByStatus(status);
+        return ResponseEntity.ok(response);
+    }
+
+    // Actualizar usuario(ADMIN o el mismo usuario)
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @userSecurityService.isOwner(#id)")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequestDTO dto) {
+        UserResponseDTO response = userService.updateUser(id, dto);
+        return ResponseEntity.ok(response);
+    }
+
+    //Activar usuario
+    @PatchMapping("/{id}/activate")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserResponseDTO> activateUser(@PathVariable Long id) {
+        userService.activateUser(id);
+        UserResponseDTO updatedUser = userService.getUserById(id);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    //Desactivar usuario
+    @PatchMapping("/{id}/deactivate")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserResponseDTO> deactivateUser(@PathVariable Long id) {
+        userService.deactivateUser(id);
+        UserResponseDTO updatedUser = userService.getUserById(id);
+        return ResponseEntity.ok(updatedUser);
+    }
+}
