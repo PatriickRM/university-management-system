e78891e3bbd6add5ba55dc2f80a5e04f0224e33e
PatriickRM <patriickrs201210@gmail.com>
2025-10-27 01:16:12 -0500

    fix provideAnimations in app.config

--- university-frontend/src/app/app.config.ts
+++ university-frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
@@ -13,6 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor, errorInterceptor])
     ),
-    provideAnimationsAsync()
+    provideAnimations()
   ]
 };
