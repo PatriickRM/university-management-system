65a0a3987a02c261c95584e172d7b534fdc36067
PatriickRM <patriickrs201210@gmail.com>
2025-10-27 16:01:51 -0500

    fix: agregar mejora de testeo al HikariCP para funcionamiento de login auth + Password generator

---
+++ backend/src/main/java/com/university/application/utils/PasswordGenerator.java
@@ -0,0 +1,9 @@
+package com.university.application.utils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin123"));
+    }
+}
